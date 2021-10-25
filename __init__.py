aebab1586e4b2ea115294ef2fc6e2c2bac59d38b
AciidSkull <zielonyfantom@interia.pl>
2021-10-25 10:21:06 +0200

    Added basic celery implementation

--- __init__.py
+++ __init__.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
+from flask_celery import make_celery
 
 app = Flask(__name__)
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
+celery = make_celery(app)
+
+@celery.task()
+def add_together(a, b):
+    return a + b
 
 @app.route('/')
 def index():
+    return f"{add_together(6,10)}"
     return render_template('index.html')
 
 if __name__ == "__main__":
