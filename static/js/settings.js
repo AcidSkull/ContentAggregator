20e6b5b0e2b6ea64c98504243afbcf96e1a3b218
AciidSkull <zielonyfantom@interia.pl>
2021-10-25 11:16:47 +0200

    Implement modal box

--- static/js/settings.js
+++ static/js/settings.js
@@ -1,5 +1,15 @@
 let settings = document.getElementById('settings');
 
 settings.addEventListener('click', () =>{
-    console.log('dfs');
+    let popup = document.getElementsByClassName('popup');
+    popup[0].style.display = 'block';
+    popup[1].style.display = 'block';
 });
+
+window.onclick = function(event){
+    if (event.target == modal){
+        let popup = document.getElementsByClassName('popup');
+        popup[0].style.display = 'none';
+        popup[1].style.display = 'none';
+    }
+}
