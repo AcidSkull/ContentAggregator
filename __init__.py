a006a1348cabf374e50252eb73c48de793eafad9
AciidSkull <zielonyfantom@interia.pl>
2021-11-02 18:06:19 +0100

    Redesign method of getting news

--- __init__.py
+++ __init__.py
@@ -4,21 +4,22 @@ from bs4 import BeautifulSoup
 import requests, psycopg2
 
 # VARIABLES
-SitesURL = ['linuxiac.com', 'news.itsfoss.com', 'eff.org', 'thenextweb.com/latest', 'digg.com/technology']
-SitesConatiner = [('h2', 'post-item-title wi-post-title fox-post-title post-header-section size-normal'),
-                ('h3', 'entry-title'),
-                ('h3', 'node__title'),
-                ('h4', 'c-listArticle__heading'),
-                ('a', 'namespace-scheme'),]
-                # ('', ''),
+SitesURL = ['linuxiac.com', 'news.itsfoss.com', 'eff.org', 'thenextweb.com/latest', 'digg.com/technology', 'cnet.com/tech/']
+SitesConatiner = [('h2', 'post-item-title wi-post-title fox-post-title post-header-section size-normal', 0),
+                ('h3', 'entry-title', 0),
+                ('h3', 'node__title', 0),
+                ('h4', 'c-listArticle__heading', 0),
+                ('a', 'namespace-scheme', 1),
+                ('div', 'item c-universalLatest_item', 2, 'h3'),]
                 # ('', ''),
                 # ('', ''),
                 # ('', ''),]
+                # [block with heading, class of heading, type of heading building]
+                # 0 - anchor in the headling
+                # 1 - headling in the anchor
+                # 2 - headlin and unwanted paragraph in the anchor
 
-# POSTGRESQL
-conn = psycopg2.connect("dbname=contaggr user=postgres")
-cur = conn.cursor()
-TablesNames = ['linuxiac', 'itsfoss', 'eff', 'thenextweb', 'digg']
+TablesNames = ['linuxiac', 'itsfoss', 'eff', 'thenextweb', 'digg', 'cnet']
 
 # FLASK and CELERY CONFIGURATION
 app = Flask(__name__)
@@ -29,34 +30,55 @@ app.config.update(
 celery = make_celery(app)
 
 @celery.task()
-def get_articles(content, index):
+def check_articles(content, index):
+    conn = psycopg2.connect("dbname=contaggr user=postgres")
+    cur = conn.cursor()
     soup = BeautifulSoup(content, 'html.parser')
     articles = soup.find_all(SitesConatiner[index][0], {"class": SitesConatiner[index][1]})
 
+    if SitesConatiner[index][2] == 0: get_articles_1(articles, index, cur)
+    elif SitesConatiner[index][2] == 1: get_articles_2(articles, index, cur)
+    elif SitesConatiner[index][2] == 2: get_articles_3(articles, index, cur)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+@celery.task()
+def get_articles_1(articles, index, cur):
     for article in articles:
         title = article.get_text().strip()
-
-        if SitesConatiner[index][0] == 'a':
-            anchor = article['href']
-        else :
-            anchor = article.find('a')['href']
+        anchor = article.find('a')['href']
 
         cur.execute(f"""INSERT INTO {TablesNames[index]} (title, anchor) VALUES (%s, %s)""", (title, anchor))
-        conn.commit()
+
+@celery.task()
+def get_articles_2(articles, index, cur):
+    for article in articles:
+        title = article.get_text().strip()
+        anchor = article['href']
+
+        cur.execute(f"""INSERT INTO {TablesNames[index]} (title, anchor) VALUES (%s, %s)""", (title, anchor))
+
+@celery.task()
+def get_articles_3(articles, index, cur):
+    for article in articles:
+        title = article.find(SitesConatiner[index][3]).get_text().strip()
+        anchor = article.find('a')['href']
+
+        cur.execute(f"""INSERT INTO {TablesNames[index]} (title, anchor) VALUES (%s, %s)""", (title, anchor))
 
 @celery.task()
 def check_for_news():
     for index, site in enumerate(SitesURL):
         page = requests.get('https://' + site)
         if page.status_code == 200:
-           get_articles(page.content, index)
+           check_articles(page.content, index)
 
 @app.route('/')
 def index():
-    cur.execute("SELECT * FROM linuxiac LIMIT 10")
-    res = cur.fetchall()
     check_for_news()
-    return render_template('index.html', res=res)
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
