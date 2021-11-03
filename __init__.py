edc43c297e0d7590f7bcafb5bf0944bf0daafbe5
AciidSkull <zielonyfantom@interia.pl>
2021-11-03 17:43:49 +0100

    Added new version of getting articles

--- __init__.py
+++ __init__.py
@@ -4,22 +4,25 @@ from bs4 import BeautifulSoup
 import requests, psycopg2
 
 # VARIABLES
-SitesURL = ['linuxiac.com', 'news.itsfoss.com', 'eff.org', 'thenextweb.com/latest', 'digg.com/technology', 'cnet.com/tech/']
+SitesURL = ['linuxiac.com', 'news.itsfoss.com', 'eff.org',
+            'thenextweb.com/latest', 'digg.com/technology', 'cnet.com/tech/',
+            'wired.com/most-recent/', 'axios.com/technology', 'techradar.com/news']
 SitesConatiner = [('h2', 'post-item-title wi-post-title fox-post-title post-header-section size-normal', 0),
                 ('h3', 'entry-title', 0),
                 ('h3', 'node__title', 0),
                 ('h4', 'c-listArticle__heading', 0),
                 ('a', 'namespace-scheme', 1),
-                ('div', 'item c-universalLatest_item', 2, 'h3'),]
-                # ('', ''),
-                # ('', ''),
-                # ('', ''),]
+                ('div', 'item c-universalLatest_item', 2, 'h3'),
+                ('li', 'archive-item-component', 2, 'h2'),
+                ('h3', 'font-regular leading-tight mt-0 md:text-h5 lg:text-h4 col-1-13 mb-12 text-h6', 0),
+                ('article', None, 3, 'h3'),]
                 # [block with heading, class of heading, type of heading building]
-                # 0 - anchor in the headling
-                # 1 - headling in the anchor
-                # 2 - headlin and unwanted paragraph in the anchor
+                # 0 - link block in the headling
+                # 1 - headling in the link block
+                # 2 - headling and unwanted tags in the link block
+                # 3 - headling and unwanted tags in the link block (but we use tag that is in the link block)
 
-TablesNames = ['linuxiac', 'itsfoss', 'eff', 'thenextweb', 'digg', 'cnet']
+TablesNames = ['linuxiac', 'itsfoss', 'eff', 'thenextweb', 'digg', 'cnet', 'wired', 'axios', 'techradar']
 
 # FLASK and CELERY CONFIGURATION
 app = Flask(__name__)
@@ -34,11 +37,16 @@ def check_articles(content, index):
     conn = psycopg2.connect("dbname=contaggr user=postgres")
     cur = conn.cursor()
     soup = BeautifulSoup(content, 'html.parser')
-    articles = soup.find_all(SitesConatiner[index][0], {"class": SitesConatiner[index][1]})
+
+    if SitesConatiner[index][1] != None:
+        articles = soup.find_all(SitesConatiner[index][0], {"class": SitesConatiner[index][1]})
+    else:
+        articles = soup.find_all(SitesConatiner[index][0])
 
     if SitesConatiner[index][2] == 0: get_articles_1(articles, index, cur)
     elif SitesConatiner[index][2] == 1: get_articles_2(articles, index, cur)
     elif SitesConatiner[index][2] == 2: get_articles_3(articles, index, cur)
+    elif SitesConatiner[index][2] == 3: get_articles_4(articles, index, cur)
 
     conn.commit()
     cur.close()
@@ -65,6 +73,16 @@ def get_articles_3(articles, index, cur):
     for article in articles:
         title = article.find(SitesConatiner[index][3]).get_text().strip()
         anchor = article.find('a')['href']
+
+        cur.execute(f"""INSERT INTO {TablesNames[index]} (title, anchor) VALUES (%s, %s)""", (title, anchor))
+
+@celery.task()
+def get_articles_4(articles, index, cur):
+    for article in articles:
+        print(article)
+        print('---------------------------------------------------------')
+        title = article.find(SitesConatiner[index][3]).get_text().strip()
+        anchor = article.find_parent('a')['href']
 
         cur.execute(f"""INSERT INTO {TablesNames[index]} (title, anchor) VALUES (%s, %s)""", (title, anchor))
 
